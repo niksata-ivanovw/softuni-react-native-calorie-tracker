bebfa11327e7fcb675109f34f9848e6c658d45fe
ShamanSoft <shamansoft@ShamanSofts-Mac-mini.local>
2026-09-02 12:52:49 +0300

    Base Structure and SafeArea

--- App.js
+++ App.js
@@ -1,34 +1,50 @@
-import { Text, Modal, View, Button, TouchableOpacity } from 'react-native';
-import { useState } from 'react';
+import { Text, Modal, View, Button, StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
-    const [show, setShow] = useState(false)
-
-    const handleShowModal = () => {
-      setShow(!show)
-    }
-
     return (
-        <View style={{paddingTop: 100, flexDirection: 'row', justifyContent: 'center', gap: 10}}>
-            <Modal
-              animationType='slide'
-              visible={show}>
-              <TouchableOpacity onPress={handleShowModal}>
-                <View style={{backgroundColor: 'red', padding: 10, borderRadius: 5}}>
-                  <Text style={{color: 'white'}}>
-                    Close Modal
-                  </Text>
-                </View>
-              </TouchableOpacity>
-            </Modal>
-
-            <TouchableOpacity onPress={handleShowModal}>
-                <View style={{backgroundColor: 'green', padding: 10, borderRadius: 5}}>
-                    <Text style={{color: 'white'}}>
-                        Show Modal
-                    </Text>
-                </View>
-            </TouchableOpacity>
+      <SafeAreaView style={styles.container}>
+        {/* Header */}
+        <View>
+          <Text>
+            Calorie Tracker
+          </Text>
         </View>
+
+        {/* Overview */}
+        <View>
+          <Text>
+            Calorie Overview
+          </Text>
+        </View>
+
+        {/* Meal Section */}
+        <View>
+          <Text>
+            List of Meals
+          </Text>
+        </View>
+
+        {/* App Bar */}
+        <View>
+          <Text>
+            App Bar
+          </Text>
+        </View>
+      
+
+        <StatusBar />
+      </SafeAreaView>
     )
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    justifyContent: 'space-between',
+  },
+});
